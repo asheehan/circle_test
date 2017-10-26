2dcba19bb0b82bda114e1fe1cb53972c9bdbad71
Andrew Sheehan <sheehan.andrew@gmail.com>
2017-10-26 12:24:17 -0500

    Revert upgrade rollout

--- .circleci/post-deploy.sh
+++ .circleci/post-deploy.sh
@@ -8,9 +8,20 @@ ssh $APP_USER@$APP_HOST <<ENDSSH
   echo "Symlinking static"
   ln -sfn lib/$APP_NAME-$APP_VERSION/priv/static static;
 
-  echo "upgrading app";
-  ./bin/$APP_NAME stop >> deploy-log.txt
-  PORT=4000 ./bin/$APP_NAME start >> deploy-log.txt
+  echo "stopping app";
+  ./bin/$APP_NAME stop >> deploy-log.txt || true;
+ENDSSH
 
-  echo "finished"
+ssh $APP_USER@$APP_HOST <<ENDSSH
+  cd ./apps/$APP_NAME;
+  date >> deploy-log.txt;
+
+  # You may want to use this if you are using Cowboy without proxy
+  # echo "giving beam permission to start the webserver"
+  # sudo setcap 'cap_net_bind_service=+ep' /home/$APP_USER/$APP_NAME/erts-9.1.2/bin/beam.smp;
+
+  echo "starting app";
+  PORT=4000 ./bin/$APP_NAME start >> deploy-log.txt;
+
+  echo "Finished";
 ENDSSH
